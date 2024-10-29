09d1f8e5886c08fee4286f45b6a4247b6c66bbb2
BogdanF <sovtour@gmail.com>
2024-10-29 09:44:47 +0100

    Initial commit

---
+++ src/TestConflict.java
@@ -0,0 +1,23 @@
+/**
+ * Group: 52-1, "AIT Hi-tech team" GMBH
+ * Author: Bogdan Fesenko
+ * Date: 29-10-2024
+ */
+/*
+
+ */
+public class TestConflict {
+
+    public void testA() {
+        //TODO implement testA
+
+        
+
+    }
+
+    public void testB() {
+        //TODO implement testB
+
+
+    }
+}
